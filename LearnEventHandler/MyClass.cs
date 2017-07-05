b864605ceb4f31a66ed103e90ecc5d20de624d59
Ratan Sunder Parai <ratanparai@gmail.com>
2017-07-05 13:03:33 +0600

    Simple EventHandler

---
+++ LearnEventHandler/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnEventHandler
+{
+    class MyClass
+    {
+        public event EventHandler OnComplete;
+
+        public void DoSomeWork()
+        {
+            // sample data
+            MyEventArgs myEventArgs = new MyEventArgs();
+            myEventArgs.Message = "This is done";
+
+            OnComplete(this, myEventArgs);
+        }
+    }
+}
---
+++ LearnEventHandler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LearnEventHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.OnComplete += new EventHandler(PrintMessage);
+            mc.DoSomeWork();
+        }
+
+        private static void PrintMessage(object sender, EventArgs e)
+        {
+            Console.WriteLine("Event fired!");
+            Console.ReadKey();
+        }
+    }
+}
